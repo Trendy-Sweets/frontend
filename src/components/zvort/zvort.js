8801a1b1190d3f84d846a1d54f3fb43a1f6a3c0c
vovkasWeb <vova77557@gmail.com>
2023-06-13 04:09:04 +0300

    fix: zvort

--- src/components/zvort/zvort.js
+++ src/components/zvort/zvort.js
@@ -6,103 +6,103 @@ import iconI from "../../img/instagram.svg";
 import icon from "../../img/exit.svg";
 function Zvort() {
   return (
-    <section class="zvorot">
-      <div class="container">
-        <div class="zvorot__inner">
+    <section className="zvorot">
+      <div className="container">
+        <div className="zvorot__inner">
           <div className="zvorot-header">
-            <h3 class="zvorot__title">Зворотній зв’язок</h3>
+            <h3 className="zvorot__title">Зворотній зв’язок</h3>
             <a className="basket__exit zvorot-exit" href="../">
               <img className="basket__img-exit" src={icon} alt="" />
             </a>
           </div>
-          <div class="zvorot__block">
-            <div class="zvorot__block-left">
+          <div className="zvorot__block">
+            <div className="zvorot__block-left">
               <input
-                class="zvorot-fio-input zvorot-input"
+                className="zvorot-fio-input zvorot-input"
                 type="text"
                 placeholder="ПIБ"
                 onChange="{handleChange}"
               />
               <input
-                class="zvorot-phon-input zvorot-input"
+                className="zvorot-phon-input zvorot-input"
                 type="text"
                 placeholder="Номер телефону"
                 onChange="{handleChange}"
               />
               <input
-                class="zvorot-phon-input zvorot-input"
+                className="zvorot-phon-input zvorot-input"
                 type="text"
                 placeholder="Електронна пошта"
                 onChange="{handleChange}"
               />
               <textarea
-                class="zvorot-info-input zvorot-input-big"
+                className="zvorot-info-input zvorot-input-big"
                 rows="10"
                 cols="45"
                 name="text"
               ></textarea>
-              <div class="check">
+              <div className="check">
                 <input type="checkbox" id="scales" name="scales" disabled />
-                <span class="checkbox__text">
+                <span className="checkbox__text">
                   Я ознайомлений із правилами Політика конфіденційності
                 </span>
               </div>
-              <button class="zvorot-button">Відправити</button>
+              <button className="zvorot-button">Відправити</button>
             </div>
-            <div class="zvorot__block-right">
-              <h4 class="zvorot__title-right">Контакти офісу</h4>
-              <div class="zvorot-phone-right">Тел. 380123456789</div>
-              <div class="zvorot-phone-right">Тел. 380123456789</div>
-              <div class="zvorot__block-sub-right">
-                <div class="zvorot__left-line">
-                  <h5 class="zvorot__title-left-line">ПРО КОМПАНІЮ</h5>
+            <div className="zvorot__block-right">
+              <h4 className="zvorot__title-right">Контакти офісу</h4>
+              <div className="zvorot-phone-right">Тел. 380123456789</div>
+              <div className="zvorot-phone-right">Тел. 380123456789</div>
+              <div className="zvorot__block-sub-right">
+                <div className="zvorot__left-line">
+                  <h5 className="zvorot__title-left-line">ПРО КОМПАНІЮ</h5>
 
                   <div>
-                    <a class="zvorot__text-left-line" href="#">
+                    <a className="zvorot__text-left-line" href="#">
                       Про нас
                     </a>
                   </div>
                   <div>
-                    <a class="zvorot__text-left-line" href="#">
+                    <a className="zvorot__text-left-line" href="#">
                       Новини
                     </a>
                   </div>
                   <div>
-                    <a class="zvorot__text-left-line" href="#">
+                    <a className="zvorot__text-left-line" href="#">
                       Відгуки та пропозиції
                     </a>
                   </div>
                   <div>
-                    <a class="zvorot__text-left-line" href="#">
+                    <a className="zvorot__text-left-line" href="#">
                       Співпраця
                     </a>
                   </div>
                   <div>
-                    <a class="zvorot__text-left-line" href="#">
+                    <a className="zvorot__text-left-line" href="#">
                       Оплата та доставка
                     </a>
                   </div>
                   <div>
-                    <a class="zvorot__text-left-line" href="#">
+                    <a className="zvorot__text-left-line" href="#">
                       Повернення та брак
                     </a>
                   </div>
                   <div>
-                    <a class="zvorot__text-left-line" href="#">
+                    <a className="zvorot__text-left-line" href="#">
                       Контакти
                     </a>
                   </div>
                   <div>
-                    <a class="zvorot__text-left-line" href="#">
+                    <a className="zvorot__text-left-line" href="#">
                       Публічна оферта
                     </a>
                   </div>
                 </div>
-                <div class="zvorot__right-line">
+                <div className="zvorot__right-line">
                   <img src={img} alt="" />
                 </div>
               </div>
-              <div class="zvorot__line-bottom">
+              <div className="zvorot__line-bottom">
                 <a href="#">
                   <img src={iconF} alt="" />
                 </a>
